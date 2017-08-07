e842a9c9dbc6082463f6b86bcc357648ed3ebc9a
Mike Calvanese <mfcalvanese@gmail.com>
2017-08-07 11:17:33 -0400

    add font-awesome

--- src/client/components/Hello.js
+++ src/client/components/Hello.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { inject, observer } from 'mobx-react'
+import fa from 'fa'
 import css from './Hello.scss'
 
 export default inject('RootStore')(observer(props => {
   const { showMessage } = props.RootStore
 
   const message = showMessage ? (
-    <FormattedMessage
-      id='Hello.message'
-      defaultMessage='Hello!'
-    />
+    <div>
+      <i className={fa.btc} />
+      <FormattedMessage
+        id='Hello.message'
+        defaultMessage='Hello!'
+      />
+    </div>
   ) : ''
 
   return (
---
+++ src/client/fa.js
@@ -0,0 +1,16 @@
+import _ from 'lodash'
+import fontAwesome from 'styles/font-awesome/scss/font-awesome.scss'
+
+const faClass = fontAwesome.fa
+
+let fa = {}
+_.forEach(fontAwesome, (faIconClass, faProp) => {
+  if (faProp !== 'fa') {
+    const faIconName = faProp.substr(3)
+    fa[faIconName] = `${faClass} ${faIconClass}`
+  }
+})
+
+console.log('FA: ', fa)
+
+export default fa
